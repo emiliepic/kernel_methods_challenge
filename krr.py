537471a87cf50d30157e7bbd9559577f22499f8d
Emilie Pic <emilie.pic@eleves.enpc.fr>
2025-03-11 11:16:31 +0100

    fixed krr

--- krr.py
+++ krr.py
@@ -8,18 +8,23 @@ class KernelRidgeRegression:
         self.alpha = None
 
     def fit(self, X, y):
+        # print(np.unique(y))
         K = self.kernel(X, X)
         n = X.shape[0]
         self.alpha = np.linalg.inv(K + self.lambd * np.eye(n)) @ y
-        support_indices = np.where(self.alpha > 1e-5)[0]
-        self.support = X[support_indices]
-        self.alpha = self.alpha[support_indices]
-        self.support_y = y[support_indices]
+        # support_indices = np.where(self.alpha > 1e-5)[0]
+        self.support = X
+        # self.alpha = self.alpha[support_indices]
+        # self.support_y = y[support_indices]
 
     def predict(self, X):
         """ Predict the y values for the input data in {-1, 1}"""
+        # print("alpha", self.alpha)
+        # print("support", self.support)
         K = self.kernel(X, self.support)
+        # print("K", K)
         f_values = K @ self.alpha
+        # print("f values ", f_values)
         return 2 * (f_values > 0) - 1
 
 
@@ -36,10 +41,10 @@ class WeightedKernelRidgeRegression:
         W_sqrt = np.sqrt(self.W)
         inv = np.linalg.inv(W_sqrt @ K @ W_sqrt + self.lambd * np.eye(n))
         self.all_alpha = W_sqrt @ inv @ W_sqrt @ y
-        support_indices = np.where(self.all_alpha > 1e-5)[0]
-        self.support = X[support_indices]
-        self.alpha = self.all_alpha[support_indices]
-        self.support_y = y[support_indices]
+        # support_indices = np.where(self.all_alpha > 1e-5)[0]
+        self.support = X #[support_indices]
+        self.alpha = self.all_alpha #[support_indices]
+        self.support_y = y #[support_indices]
     
     def predict(self, X):
         """ Predict the y values for the input data in {-1, 1}"""
--- run.py
+++ run.py
@@ -15,11 +15,10 @@ from krr import KernelRidgeRegression, WeightedKernelRidgeRegression
 from log_reg import KernelLogisticRegression
 
 do_pca = False
-using_julia = True
-best_params_tot = {}
-text_date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
-model_class = "svc"
-encoding = "int"
+using_julia = False
+
+# model_class = "svc"
+# encoding = "int"
 
 kernel_params = {
     'gaussian': {'sigma': [0.01, 0.1, 1, 10]},
@@ -34,102 +33,108 @@ param_map = {
     "wkrr": {'lambd': [0.1, 1, 10], 'W': [0.1, 2, 10]},
     "log_reg": {'lambd': [0.1, 1, 10]}
 }
-
-for kernel_type in ['linear', 'gaussian', 'Poly', 'Sigmoid']:  # 'linear', 'gaussian', 'Poly', 'Sigmoid'
-    best_params_tot[kernel_type] = {}
-    Y_pred_tot = []
-    score_val_tot = 0
-
-    # Set model-specific params
-    params = param_map.get(model_class)
-    if not params:
-        raise ValueError("Invalid model class")
-
-    # Set kernel-specific params
-    kernel_params_for_type = kernel_params.get(kernel_type)
-    if kernel_params_for_type:
-        params.update(kernel_params_for_type)
-    
-    # Generate parameter grid
-    param_grid_list = []
-    for param, values in params.items():
-        param_grid_list.append(values)
-        
-    params_grid = [dict(zip(params.keys(), comb)) for comb in itertools.product(*param_grid_list)]
-
-
-    for k in range(3):
-        X_numpy, Y_numpy = load_data(k, "train", encoding=encoding)
-        # X_numpy = X_numpy[:200]
-        # Y_numpy = Y_numpy[:200]
-        X_test = load_data(k, "test", encoding=encoding)
-        
-        # PCA on the data
-        if do_pca:
-            pca = PCA(n_components=2)
-            X_train_pca = pca.fit_transform(X_numpy)
-            print("PCA done")
-            X_te_pca = pca.transform(X_test)
-        else:
-            X_train_pca = X_numpy
-            X_te_pca = X_test
-    
-        
-        best_params, best_score = k_fold_cross_validation(X_train_pca, Y_numpy, kernel_type, model_class, params_grid, k=3, using_julia=using_julia)
-        best_params_tot[kernel_type][k] = best_params
-        print(f"Best parameters for model {kernel_type} on dataset {k} : {best_params}")
-        
-        X_train, X_val, Y_train, Y_val = train_test_split(X_train_pca, Y_numpy, test_size=0.2, random_state=42)
-        if kernel_type == 'gaussian':
-            kernel = RBF(sigma=best_params['sigma']).kernel
-        elif kernel_type == "linear":
-            kernel = Linear().kernel
-        elif kernel_type == "Poly":
-            kernel = Polynomial(degree=best_params['degree'], r=best_params['r']).kernel
-        elif kernel_type == "Sigmoid":
-            kernel = Sigmoid(sigma=best_params['sigma'], r=best_params['r']).kernel
-        
+for model_class in ['krr', 'log_reg', 'svc']:  # 'svc', 'krr', 'wkrr', 'log_reg'
+    for encoding in ['bin' , 'int', 'one_hot']: 
+        best_params_tot = {}
+        text_date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
         if model_class == "svc":
-            model = KernelSVC(C=best_params['C'], kernel=kernel, epsilon=1e-14)
-        elif model_class == "krr":
-            model = KernelRidgeRegression(lambd=best_params['lambd'], kernel=kernel, epsilon=1e-14)
-        elif model_class == "wkrr":
-            n = len(Y_train)
-            W_matrix = np.diag([best_params['W'] for _ in range(n)])
-            model = WeightedKernelRidgeRegression(lambd=best_params['lambd'], kernel=kernel, W=W_matrix, epsilon=1e-14)
-        elif model_class == "log_reg":
-            model = KernelLogisticRegression(kernel=kernel, lambd=best_params['lambd'])
+            using_julia = True
+        for kernel_type in ['Sigmoid','linear', 'gaussian', 'Poly']:  # 'linear', 'gaussian', 'Poly', 'Sigmoid'
+            best_params_tot[kernel_type] = {}
+            Y_pred_tot = []
+            score_val_tot = 0
+
+            # Set model-specific params
+            params = param_map.get(model_class)
+            if not params:
+                raise ValueError("Invalid model class")
+
+            # Set kernel-specific params
+            kernel_params_for_type = kernel_params.get(kernel_type)
+            if kernel_params_for_type:
+                params.update(kernel_params_for_type)
+            
+            # Generate parameter grid
+            param_grid_list = []
+            for param, values in params.items():
+                param_grid_list.append(values)
+                
+            params_grid = [dict(zip(params.keys(), comb)) for comb in itertools.product(*param_grid_list)]
+
+            print(f"Model class: {model_class}, encoding: {encoding}, kernel type: {kernel_type}, params: {params_grid}")
+            for k in range(3):
+                X_numpy, Y_numpy = load_data(k, "train", encoding=encoding)
+                # X_numpy = X_numpy[:200]
+                # Y_numpy = Y_numpy[:200]
+                X_test = load_data(k, "test", encoding=encoding)
+                
+                # PCA on the data
+                if do_pca:
+                    pca = PCA(n_components=2)
+                    X_train_pca = pca.fit_transform(X_numpy)
+                    print("PCA done")
+                    X_te_pca = pca.transform(X_test)
+                else:
+                    X_train_pca = X_numpy
+                    X_te_pca = X_test
+            
+                
+                best_params, best_score = k_fold_cross_validation(X_train_pca, Y_numpy, kernel_type, model_class, params_grid, k=3, using_julia=using_julia)
+                best_params_tot[kernel_type]["score"] = best_score
+                best_params_tot[kernel_type][k] = best_params
+                print(f"Best parameters for model {kernel_type} on dataset {k} : {best_params}")
+                
+                X_train, X_val, Y_train, Y_val = train_test_split(X_train_pca, Y_numpy, test_size=0.2, random_state=42)
+                if kernel_type == 'gaussian':
+                    kernel = RBF(sigma=best_params['sigma']).kernel
+                elif kernel_type == "linear":
+                    kernel = Linear().kernel
+                elif kernel_type == "Poly":
+                    kernel = Polynomial(degree=best_params['degree'], r=best_params['r']).kernel
+                elif kernel_type == "Sigmoid":
+                    kernel = Sigmoid(sigma=best_params['sigma'], r=best_params['r']).kernel
+                
+                if model_class == "svc":
+                    model = KernelSVC(C=best_params['C'], kernel=kernel, epsilon=1e-14)
+                elif model_class == "krr":
+                    model = KernelRidgeRegression(lambd=best_params['lambd'], kernel=kernel, epsilon=1e-14)
+                elif model_class == "wkrr":
+                    n = len(Y_train)
+                    W_matrix = np.diag([best_params['W'] for _ in range(n)])
+                    model = WeightedKernelRidgeRegression(lambd=best_params['lambd'], kernel=kernel, W=W_matrix, epsilon=1e-14)
+                elif model_class == "log_reg":
+                    model = KernelLogisticRegression(kernel=kernel, lambd=best_params['lambd'])
 
 
-        print("Model created, fitting started")
-        if using_julia:
-            model.fit_julia(X_train, Y_train)
-        else:
-            model.fit(X_train, Y_train)
-        print("Model fitted")
-        Y_pred_train = model.predict(X_train)
-        Y_pred_val = model.predict(X_val)
-        Y_pred_test = model.predict(X_te_pca)
-        Y_pred_tot.append(Y_pred_test)
-        print(f'F1 score for fold {k} on train is {f1_score_m1_1(Y_train, Y_pred_train)}')
-        print(f'F1 score for fold {k} on val is {f1_score_m1_1(Y_val, Y_pred_val)}')
-        score_val_tot += f1_score_m1_1(Y_val, Y_pred_val)
+                print("Model created, fitting started")
+                if using_julia:
+                    model.fit_julia(X_train, Y_train)
+                else:
+                    model.fit(X_train, Y_train)
+                print("Model fitted")
+                Y_pred_train = model.predict(X_train)
+                Y_pred_val = model.predict(X_val)
+                Y_pred_test = model.predict(X_te_pca)
+                Y_pred_tot.append(Y_pred_test)
+                print(f'F1 score for fold {k} on train is {f1_score_m1_1(Y_train, Y_pred_train)}')
+                print(f'F1 score for fold {k} on val is {f1_score_m1_1(Y_val, Y_pred_val)}')
+                score_val_tot += f1_score_m1_1(Y_val, Y_pred_val)
 
-    print(f'Average F1 score on validation set : {score_val_tot/3}')
-    score_val_tot /= 3
-    Y_pred_tot = np.array(Y_pred_tot)
-    # transformer les -1 en 0
-    Y_pred_tot[Y_pred_tot == -1] = 0
-    Y_pred_tot = Y_pred_tot.ravel()
-    # save to csv file with header Id,Bound
-    df = pd.DataFrame({'Id': np.arange(len(Y_pred_tot)), 'Bound': Y_pred_tot})
-    # text_params = '_'.join([f'{key}_{value}' for key, value in best_params.items()])
-    df.to_csv(f'results/{text_date}_Yte_{encoding}_{model_class}_pca{do_pca}_{kernel_type}_{score_val_tot:.3f}.csv', index=False)
+            print(f'Average F1 score on validation set : {score_val_tot/3}')
+            score_val_tot /= 3
+            Y_pred_tot = np.array(Y_pred_tot)
+            # transformer les -1 en 0
+            Y_pred_tot[Y_pred_tot == -1] = 0
+            Y_pred_tot = Y_pred_tot.ravel()
+            # save to csv file with header Id,Bound
+            df = pd.DataFrame({'Id': np.arange(len(Y_pred_tot)), 'Bound': Y_pred_tot})
+            # text_params = '_'.join([f'{key}_{value}' for key, value in best_params.items()])
+            df.to_csv(f'results/{text_date}_Yte_{encoding}_{model_class}_pca{do_pca}_{kernel_type}_{score_val_tot:.3f}.csv', index=False)
 
 # save best params to json file
 
-with open(f'results/{text_date}_best_params_tot.json', 'w') as f:
-    json.dump(best_params_tot, f)
+        with open(f'results/{text_date}_best_params_tot.json', 'w') as f:
+            json.dump(best_params_tot, f)
 
 
 # for kernel_type in ['linear']: #'gaussian', 'Poly', 'Sigmoid'
